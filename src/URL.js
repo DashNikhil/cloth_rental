45095e2e1a522f886cb9b8ef103818ee2106be82
DashNikhil <nkkushwaha8948@gmail.com>
2026-05-03 22:28:12 +0530

    backend-url-update

--- src/URL.js
+++ src/URL.js
@@ -1,1 +1,5 @@
-export const baseURL = "https://cloth-rental-backend-1f3y.onrender.com";
+// export const baseURL = "https://cloth-rental-backend-1f3y.onrender.com";
+// export const baseURL = "https://cloth-rental-backend-1f3y.onrender.com";
+export const baseURL = "https://cloth-rental-backend-qmtb.onrender.com";
+
+// export const baseURL = "http://127.0.0.1:8080";
