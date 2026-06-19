2758322e0e752a1c4aa028310faa49daad3b9716
DashNikhil <nkkushwaha8948@gmail.com>
2026-06-20 00:38:33 +0530

    url

--- src/URL.js
+++
@@ -1,5 +0,0 @@
-// export const baseURL = "https://cloth-rental-backend-1f3y.onrender.com";
-// export const baseURL = "https://cloth-rental-backend-1f3y.onrender.com";
-export const baseURL = "https://cloth-rental-backend-qmtb.onrender.com";
-
-// export const baseURL = "http://127.0.0.1:8080";
