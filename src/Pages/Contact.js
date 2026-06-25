8e4e8f003fa3c786fe5b91f92386e69684c7bc29
DashNikhil <nkkushwaha8948@gmail.com>
2026-06-25 12:02:09 +0530

    contact and navbar and footer update

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -154,24 +154,12 @@ const Contact = () => {
         </button>
       </div>
 
-      <div
-        style={{
-          width: "100%",
-          height: "65vh",
-          display: "flex",
-          justifyContent: "center",
-          marginBottom: "30px",
-        }}
-      >
+      <div className="cnt-mp">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3566.2478577720744!2d80.93562247521449!3d26.640545476810484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399bf1042871748f%3A0xde0c16769f0166f4!2sAmbalika%20Institute%20Of%20Management%20And%20Technology%20Campus%2C%20Lucknow%2C%20Uttar%20Pradesh!5e0!3m2!1sen!2sin!4v1702715942689!5m2!1sen!2sin"
           title="map"
+          className="cnt-mp-ifm"
           style={{
-            border: "0",
-            height: "100%",
-            margin: "2px auto",
-            width: "85%",
-            textAlign: "center",
             loading: "lazy",
             referrerpolicy: "no-referrer-when-downgrade",
           }}
