739c9d198fbbacc8ba79a1dcfeb7feee9616d89b
DashNikhil <nkkushwaha8948@gmail.com>
2026-07-09 02:13:07 +0530

    distance page updated

--- src/components/Distance.js
+++ src/components/Distance.js
@@ -13,6 +13,7 @@ const Distance = () => {
   const [ptime, setPtime] = useState("");
   const [d_date, setD_date] = useState("");
   const [d_time, setD_time] = useState("");
+  const [typ, setTyp] = useState(true);
 
   const CheckData = (e) => {
     e.preventDefault();
@@ -111,10 +112,11 @@ const Distance = () => {
               Pick-up
             </div>
             <input
-              type="date"
+              type={typ ? "text" : "date"}
               placeholder="dd-mm-yyyy"
               onChange={(e) => setPdate(e.target.value)}
               value={pdate}
+              onClick={() => setTyp(false)}
             />
             <select
               style={{ marginLeft: "5px" }}
